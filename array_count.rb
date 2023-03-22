751f366331830d60d7765d34a3380f14aa794589
William Price <price.james.william@gmail.com>
2023-03-22 20:18:51 +0000

    added solutions for count and square

--- array_count.rb
+++ array_count.rb
@@ -1,7 +1,7 @@
 # Ask the user to enter at least 2 numbers and store their input.
-# The starting code below does this for you. 
+# The starting code below does this for you.
 # Use the `count` method to prtin how many numbers the user entered.
-# 
+#
 # Expected output:
 #   "Enter at least 2 numbers, separated by spaces:"
 #   42 5 3 8 # user's input
@@ -12,3 +12,5 @@ p "Enter at least 2 numbers, separated by spaces:"
 user_string = gets.chomp
 
 user_numbers = user_string.split
+
+p user_numbers.count
--- array_element_square.rb
+++ array_element_square.rb
@@ -1,13 +1,17 @@
 # Print the square of the second-to-last number that the user enters.
-# 
+#
 # The expected output will be something like:
-# 
+#
 # Enter at least 2 numbers, separated by spaces:
 # 9.0
-# 
+#
 
 p "Enter at least 2 numbers, separated by spaces:"
 
 user_string = gets.chomp
 
 user_numbers = user_string.split
+
+answer = user_numbers.at(-2)
+
+puts answer.to_i ** 2
